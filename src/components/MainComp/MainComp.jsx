f5315a2acca0e534fbeea1b377c1b1ab092b19a7
Zain Ullah <zainullahachakzai@gmail.com>
2025-06-30 16:58:53 +0500

    meme new display

--- src/components/MainComp/MainComp.jsx
+++ src/components/MainComp/MainComp.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import styles from './MainComp.module.css'
+import logo from "../../assets/logo.png"
 
 export default function MainComp() {
     return(
         <main>
             <div className={styles.form}>
-                <label htmlFor="toptext">Top Text</label>
-                <input type="text" id="toptext" />
+                <label htmlFor="toptext">Top Text
+                    <input type="text" id="toptext" />
+                </label>
 
-                <label htmlFor="bottomtext">Bottom Text</label>
-                <input type="text" id="bottomtext" />
+                <label htmlFor="bottomtext">Bottom Text
+                    <input type="text" id="bottomtext" />
+                </label>
 
-                <button>Get a new meme Image</button>
+                <button className={[styles.btnGenerator, styles.rowSpan2].join(" ")}>Get a new meme Image</button>
             </div>
 
-            <div className="meme">
-
+            <div className={styles.meme}>
+                {/* <img src={logo}  height="100px" alt="" srcset="" /> */}
             </div>
         </main>
     )
