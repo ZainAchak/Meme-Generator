f802bbca660e6ac869bae92bb655d9046420f444
Zain Ullah <zainullahachakzai@gmail.com>
2025-07-01 19:44:36 +0500

    getting images from meme api

--- src/components/MainComp/MainComp.jsx
+++ src/components/MainComp/MainComp.jsx
@@ -1,20 +1,39 @@
 import React, { useEffect, useState } from "react";
 import styles from './MainComp.module.css'
 
+function getRandomNumber() {
+  return Math.floor(Math.random() * 100);
+}
+
 export default function MainComp() {
     const [meme, setMeme] = useState({
         topText: "",
         bottonText: "",
         imageUrl: "http://i.imgflip.com/1bij.jpg"
         })
+    const memeAPI = "https://api.imgflip.com/get_memes";
+    const [memesData,setMemesData ]= useState(null);
 
-    // useEffect(()=>{},[])
-
+    
+    useEffect(()=>{
+        fetch(memeAPI)
+                .then((data)=>data.json())
+                .then((dataJson)=>{
+                    setMemesData(dataJson.data.memes)
+                })
+                .catch((err)=> console.error("Error fetching memes",err));
+        },[])
+    
     function handleKeyTop(event) {
         const {id,value} = event.currentTarget;
         setMeme(prevMeme => ({...prevMeme,[id]:value}))
     }
 
+    function generateNewImage() {
+        const randonNumber = getRandomNumber();
+        setMeme(prevMeme => ({...prevMeme,imageUrl:memesData[randonNumber].url}))
+    }
+    console.log(memesData)
     return(
         <main>
             <div className={styles.form}>
@@ -36,7 +55,9 @@ export default function MainComp() {
                         value={meme.bottonText} />
                 </label>
 
-                <button className={[styles.btnGenerator, styles.rowSpan2].join(" ")}>Get a new meme Image</button>
+                <button 
+                    onClick={generateNewImage} 
+                    className={[styles.btnGenerator, styles.rowSpan2].join(" ")}>Get a new meme Image</button>
             
                 <div className={[styles.meme,styles.rowSpan2].join(" ")}>
                     <img src={meme.imageUrl}/>
