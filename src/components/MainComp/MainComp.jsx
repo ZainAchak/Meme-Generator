7d0f5d732b48b1dcbb2966cefca4f57d9fa0863f
Zain Ullah <zainullahachakzai@gmail.com>
2025-07-01 16:43:14 +0500

    new updates to meme

--- src/components/MainComp/MainComp.jsx
+++ src/components/MainComp/MainComp.jsx
@@ -1,25 +1,52 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styles from './MainComp.module.css'
-import logo from "../../assets/logo.png"
 
 export default function MainComp() {
+    const [meme, setMeme] = useState({
+        topText: "",
+        bottonText: "",
+        imageUrl: "http://i.imgflip.com/1bij.jpg"
+        })
+
+    // useEffect(()=>{},[])
+
+    function handleKeyTop(event) {
+        const {id,value} = event.currentTarget;
+        setMeme(prevMeme => ({...prevMeme,[id]:value}))
+    }
+
     return(
         <main>
             <div className={styles.form}>
                 <label htmlFor="toptext">Top Text
-                    <input type="text" id="toptext" />
+                    <input 
+                        onChange={handleKeyTop} 
+                        type="text" 
+                        id="topText"
+                        placeholder="One does not simply"
+                        value={meme.topText} />
                 </label>
 
                 <label htmlFor="bottomtext">Bottom Text
-                    <input type="text" id="bottomtext" />
+                    <input 
+                        onChange={handleKeyTop}
+                        type="text" 
+                        id="bottonText"
+                        placeholder="debug on Friday."
+                        value={meme.bottonText} />
                 </label>
 
                 <button className={[styles.btnGenerator, styles.rowSpan2].join(" ")}>Get a new meme Image</button>
+            
+                <div className={[styles.meme,styles.rowSpan2].join(" ")}>
+                    <img src={meme.imageUrl}/>
+                    <span className={styles.top}>{meme.topText}</span>
+                    <span className={styles.bottom}>{meme.bottonText}</span>
+                </div>
+
             </div>
 
-            <div className={styles.meme}>
-                {/* <img src={logo}  height="100px" alt="" srcset="" /> */}
-            </div>
+            
         </main>
     )
 }
